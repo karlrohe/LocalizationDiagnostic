8f2b5da7fd61c22ab0ef079cc8c58e62c0cf3f8d
karlrohe <karlrohe@stat.wisc.edu>
2020-07-19 09:05:40 -0500

    plot very low degree nodes too.

--- KeyFunctionsForDiagnosticPlots.R
+++ KeyFunctionsForDiagnosticPlots.R
@@ -6,22 +6,26 @@ library(tidyverse)
 # if you have a massive graph, then you might want to subsample
 #  to make the plot faster to build.  Be careful with this... you could
 #  miss the pattern.  If you want to do this, uncomment the middle lines of code.
-plotDegLev= function(deg, lev, n = 100000){
-  x = deg[deg>5]
-  y = lev[deg>5]
-  samp = 1:length(x)  # this will be overwrote and a subsample will be taken if you uncomment the next lines.
+plotDegLev= function(degree, leverage, n = 100000){
+  leverage = leverage[degree>0]
+  degree= degree[degree>0]
+  m = min(leverage[degree>5])
+  M = max(leverage)
+  samp = 1:length(degree)  # this will be overwrote and a subsample will be taken if you uncomment the next lines.
   #   # samp1 biases to high leverage/degree points.
   #   # samp2 is uniform sample
-  # samp1 = sample(length(x), size = min(length(x), n)/2, prob = x+y)
-  # samp2 = sample(length(x), size = min(length(x), n)/2)
+  # samp1 = sample(length(degree), size = min(length(degree), n)/2, prob = degree + leverage)
+  # samp2 = sample(length(degree), size = min(length(degree), n)/2)
   # samp is a mixture of both.
   # samp = c(samp1, samp2)  
-  tibble(degree = x[samp], leverage = y[samp]) %>% 
+  tibble(degree = degree[samp], leverage = leverage[samp]) %>% 
     ggplot(aes(x=degree, y= leverage)) +
     geom_point(alpha = .05)+
-    geom_smooth(se = F, method = "gam") + 
-    scale_x_log10() + scale_y_log10()
+    geom_smooth(se = F, method = "gam")+ 
+    ylim(m,M) +
+    scale_x_log10() + scale_y_log10() 
 }
+
 
 
 # this returns the eigs of the regularized laplacian.
